beeb839375eea85f3ee431f63fdeded15444a981
Khaing ccn <ccn.khaing@yahoo.com.sg>
2014-01-16 05:04:47 -0600

    Changes made by Khaing ccn (ccn.khaing@yahoo.com.sg).

--- CCN.FreightQuotation/scripts/rfqservice.js
+++ CCN.FreightQuotation/scripts/rfqservice.js
@@ -6,29 +6,33 @@ var DataType;
 var ProcessData;
 var Method;
 
-ContentType = "application/json; charset=utf-8";
+ContentType = "application/json";
 DataType = "json";
 
 //Generic function to call WCF  Service
-function CallService() {
-   $.ajax({
-      type: Type, //GET or POST or PUT or DELETE verb
-      url: Url, // Location of the service
-      data: Data, //Data sent to server      
-      processdata: ProcessData, //True or False
-      success: function(msg) {//On Successfull service call        
-       ServiceSucceeded(msg);
-      },
-   error: ServiceFailed// When Service call fails
-  });
- }
+function callService() {
+    $.ajax({
+        cache: false,
+        async: false,
+        contentType : ContentType,
+        dataType:DataType,
+        type: Type,
+        url: Url,
+        data: Data,      
+        processdata: ProcessData,
+        success: function(msg) {        
+            serviceSucceeded(msg);
+        },
+        error: serviceFailed
+    });
+}
  
-function ServiceFailed(result) {
-   alert('Service call failed: ' + result.status + '' + result.statusText);
-   Type = null; 
-   Url = null; 
-   Data = null; 
-   ContentType = null; 
-   DataType = null; 
-   ProcessData = null;
+function serviceFailed(result) {
+    alert('Service call failed: ' + result.status + '' + result.statusText);
+    Type = null; 
+    Url = null; 
+    Data = null; 
+    ContentType = null; 
+    DataType = null; 
+    ProcessData = null;
 }
