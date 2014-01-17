9c1209051daa2f6d2938f4b5e801118965f60d2b
Khaing ccn <ccn.khaing@yahoo.com.sg>
2014-01-17 02:33:13 -0600

    Changes made by Khaing ccn (ccn.khaing@yahoo.com.sg).

--- CCN.FreightQuotation/scripts/main.js
+++ CCN.FreightQuotation/scripts/main.js
@@ -84,7 +84,7 @@ quotationApp.prototype = function() {
     },
     
     _initHome = function(){
-        //alert("init home");
+       //alert("init home");
        rfqData.getDataforRFQSummary(_ffNum,_handleDataForRFQSummary);
     },
     _initCheckIn = function(){
@@ -143,11 +143,11 @@ quotationApp.prototype = function() {
 		//$('#numberOfFlights').text(data.flights.length);
 		for (var i in data.requestQuotation) {
             var rfq = data.requestQuotation[i];
-           $myRFQSummary.append('<div class="ui-corner-all custom-corners"><div class="ui-bar ui-bar-c"><a href="viewRFQDetail.html" rel="external"><h3>RFQ ' + i + 1 + '</h3></a></div><div class="ui-body ui-body-c"><table><tr><th>'
+           $myRFQSummary.append('<div><a href="viewRFQDetail.html" rel="external"><div class="ui-corner-all custom-corners"><div class="ui-bar ui-bar-c"><h3>RFQ ' + i + 1 + '</h3></div><div class="ui-body ui-body-c"><table><tr><th>'
             + rfq.origin + ' - ' + rfq.destination + '</th><th>&nbsp;<th>&nbsp;</th><th>' + rfq.status + '</th></tr><tr><td colspan="4">&nbsp;' + rfq.goodsDescription 
             + '</td></tr><tr><td>&nbsp;' + rfq.commodity + '</td><td>' + rfq.pieces + ' pcs</td><td>&nbsp;' + rfq.weight + ' kg</td><td>&nbsp;' + rfq.volume + ' cubic m</td></tr><tr><td>&nbsp;' 
             + 'Created Date<br />' + rfq.createDT + '</td><td>LogeIn Date<br />' + rfq.LogeInDT + '<br /></td><td>&nbsp;Collection Date<br />' + rfq.collectionDT + '<br /></td><td>&nbsp;Expiry Date<br />' 
-            + rfq.expireOn + '</td></tr></table></div></div><br />');
+            + rfq.expireOn + '</td></tr></table></div></div></div></a><br />');
 		}
 		//$.mobile.changePage('main.html#home', { transition: 'flip' });
        //window.location.href="main.html#home";
--- CCN.FreightQuotation/scripts/rfqdata.js
+++ CCN.FreightQuotation/scripts/rfqdata.js
@@ -68,10 +68,10 @@ rfqSummaryData.prototype = function() {
 		]
 	},
     
-	getDataforRFQSummary = function(id, callback) {
-		fauxAjax(function () {
+	getDataforRFQSummary = function(id, callback) {        
+		//fauxAjax(function () {
 			callback(requestSummary);
-		}, 'loading data ...', this);
+		//}, 'loading data ...', this);
 	},
     
 	logOn = function (uid, pwd, callback) {
@@ -80,7 +80,7 @@ rfqSummaryData.prototype = function() {
 		}, 'logging in ...', this);
 	},
     
-	fauxAjax = function fauxAjax(func, text, thisObj) {
+	fauxAjax = function fauxAjax(func, text, thisObj) {       
 		$.mobile.loading('show', { theme: 'a', textVisible: true, text:text });
 		window.setTimeout(function () {
 			$.mobile.loading('hide');
